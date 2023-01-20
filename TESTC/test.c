bb9fa62dc445d03129038e9ea1fd94876d898b79
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2023-01-20 12:44:52 -0300

    Testando funções basicas

--- TESTC/test.c
+++ TESTC/test.c
@@ -7,5 +7,21 @@
 
 void main(void) 
 {
-  Print("Teste de compilacao!");
+  char c,*frase;
+  Print("Teste de compilacao!\n");
+  PrintNumber(100);
+  Print("\n");
+  PrintFNumber(100,48,5);
+  Print("\n");
+  PrintChar(60);
+  PrintChar('<');
+  PrintDec(-125);
+  Print("\n");
+  Print("Digite um caracter:");
+  c = InputChar();
+  Print("\nVoce digitou=>");
+  PrintChar(c);
+  Print("Digite uma palavra de oito letras:");
+  InputString(frase,8);
+  PrintString(frase);
 }
