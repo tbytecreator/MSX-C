0d20ccbc068f35235e896518327c5eba13190a43
tbytecreator <tbytecreator@gmail.com>
2024-02-11 20:25:16 -0300

    Demonstração de operadores no Fusion-C

---
+++ OPERADORES/OP.C
@@ -0,0 +1,130 @@
+// -----------------------------------
+// DEMONSTRACAO DE OPERADORES EM C
+// MANOEL NETO 2024-02-11
+// -----------------------------------
+
+// OS OPERADORES SAO UTILIZADOS PARA 
+// REALIZAR OPERAÇÕES MATEMÁTICAS E 
+// LÓGICAS EM C.
+
+#include <stdio.h>
+#include "fusion-c/header/msx_fusion.h"
+
+// OPERADORES ARITMETICOS
+// +	ADICAO
+// −	SUBTRACAO 
+// *	MULTIPLICACAO
+// /	DIVISAO 
+// %	RESTO DA DIVISAO 
+// ++	INCREMENTO
+// --	DECREMENTO 
+void aritimeticos(int a, int b)
+{
+    int r;
+    Print("\n");
+
+    // Adicao
+    r = a + b;
+    Print("Adicao:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Subtracao
+    r = a - b;
+    Print("Subtracao:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Multiplicacao
+    r = a * b;
+    Print("Multiplicacao:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Divisao
+    r = a / b;
+    Print("Divisao:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Resto da Divisao
+    r = a % b;
+    Print("Resto da Divisao:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Incremento
+    r = a;
+    r++;
+    Print("Incremento:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Decremento
+    r = a;
+    r--;
+    Print("Decremento:");
+    PrintNumber(r);
+    Print("\n");
+}
+
+// OPERADORES RELACIONAIS
+// ==	IGUAL
+// !=	DIFER
+// >	MAIOR
+// <	MENOR
+// >=	MAIOR OU IGUAL
+// <=	MENOR OU IGUAL
+void relacionais(int a, int b)
+{
+    int r;
+    Print("\n");
+
+    // Igual
+    r = a == b;
+    Print("Igual:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Diferente
+    r = a != b;
+    Print("Diferente:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Maior
+    r = a > b;
+    Print("Maior:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Menor
+    r = a < b;
+    Print("Menor:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Maior ou Igual
+    r = a >= b;
+    Print("Maior ou Igual:");
+    PrintNumber(r);
+    Print("\n");
+
+    // Menor ou Igual
+    r = a <= b;
+    Print("Menor ou Igual:");
+    PrintNumber(r);
+    Print("\n");
+}
+
+void main(void)
+{
+    int a = 10;
+    int b = 5;
+
+    // testa os operadores aritimeticos
+    aritimeticos(a,b);
+
+    // testa os operadores relacionais
+    relacionais(a,b);
+}
