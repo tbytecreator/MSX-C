b7eda0ad0c26117e210755032076f15e91d33aef
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2025-01-28 19:56:26 -0300

    Iniciar o app pelo linux

--- APPS/MENU/openMSX/emul_start.sh
+++ APPS/MENU/openMSX/emul_start.sh
@@ -1,7 +1,29 @@
-machine panasonic_fs-a1st 
-ext msxdos2 
-ext gfx9000 
-bind F12 cycle videosource
-plug joyporta mouse 
-plug printerport simpl 
-diska dsk/ 
+# OpenMSX Start Script
+# The emulator will be started only if it's not already active
+
+#MacOS
+if [ "$(uname -s)" = "Darwin" ]; then
+	xopenmsx=`ps x | grep "openmsx " | grep -v grep`
+	 
+	if [ "${xopenmsx}" = "" ]; then
+	  echo "...Now Starting OpenMsx "
+	  ./openMSX/openMSX.app/Contents/MacOS/openmsx -script ./openMSX/emul_start_config.txt | NULL
+	else
+	  echo "...openMSX already running "
+	fi
+
+#Linux
+elif [ "$(uname -s)" = "Linux" ]; then
+	xopenmsx=`pgrep "openmsx"`
+	
+	if [ "${xopenmsx}" = "" ]; then
+		echo "...Now Starting OpenMSX "
+		/usr/bin/openmsx -script ./openMSX/emul_start_config.txt
+	else
+		echo "...openMSX already running "
+	fi
+
+#Other
+else
+	echo "Failed to identify OS"
+fi
