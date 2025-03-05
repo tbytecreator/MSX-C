1591d7435a4d1be16101575a8414d847cbd8a45b
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2025-03-04 23:58:05 -0300

    Acertos de aula

--- APPS/MENU/MENU.C
+++ APPS/MENU/MENU.C
@@ -8,15 +8,15 @@ typedef struct {
     char *texto;
 } MenuItem;
 
-MenuItem *menuItems = NULL;
-int menuItemCount = 0;
+// globals
+static FCB file;
 
-void AddMenuItem(const char *text, int x, int y) 
+/* void AddMenuItem(const char *text, int x, int y) 
 {
     menuItems = (MenuItem *)realloc(menuItems, (menuItemCount + 1) * sizeof(MenuItem));
     menuItems[menuItemCount].texto = strdup(text);
     menuItemCount++;
-}
+} */
 
 void DrawBox(int x, int y, int width, int height) 
 {
@@ -65,32 +65,18 @@ void DrawBox(int x, int y, int width, int height)
 
 void ReadFiles()
 {
-    struct FCB fcb;
-    char filename[13];
-
-    // Abre o diretório atual
-    int handle = opendir("");
-
-    if (handle == -1) {
-        printf("Erro ao abrir o diretório.\n");
-        return;
-    }
-
-    // Lê os arquivos do diretório
-    while (readdir(handle, &fcb) != EOF)
+    // Variable
+    char sbuf[255];
+    int n;
+  
+    // User interface 
+    n=FindFirst("*.ROM",sbuf,0);
+    for(;!n;)
     {
-        // Obtém o nome do arquivo
-        strncpy(filename, fcb.fname, 12);
-        filename[12] = '\0'; // Garante que a string esteja terminada corretamente
-
-        // Verifica se o arquivo termina com .ROM
-        if (strcasecmp(&filename[strlen(filename) - 4], ".rom") == 0) 
-        {
-            printf("%s\n", filename);
-        }
+        Print(sbuf);
+        Print("\n");
+        n=FindNext(sbuf);
     }
-
-    closedir(handle); // Fecha o diretório
 }
 
 void main(void)
@@ -102,11 +88,12 @@ void main(void)
 
     // desenha o menu
     DrawBox(0,0,40,2);
-    Locate(1,1);Print("TBC EXECUTOR");
+    Locate(1,1);Print("TBC SPIDER FLASH WRITER");
     DrawBox(0,3,40,15);
     DrawBox(0,19,40,2);
     Locate(1,20);Print("ESCOLHA UM ARQUIVO PARA GRAVAR");
 
     // le os arquivos do diretorio
+    Locate(1,4);
     ReadFiles();
 }
