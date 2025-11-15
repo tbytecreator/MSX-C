02fb30ec677a8d140f38428f9e241d250916531d
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2025-11-15 16:36:03 -0300

    Acertos de aula.

--- AULAS/LOOPS/LACOS.C
+++ AULAS/LOOPS/LACOS.C
@@ -2,24 +2,45 @@
 # include <stdlib.h>
 # include "fusion-c/header/msx_fusion.h"
 
-void main(void)
+int lenumero()
 {
     char buffer[10];
-    int numero;
-    
-    // Limpa a tela
-    Screen(0);
-    
+    int numero;  
     // Exibe mensagem
-    Print("Digite um numero: ");
-    
+    Print("\nDigite um numero: ");
     // Lê a string
     InputString(buffer, 10);
-    
     // Converte string para inteiro
     numero = atoi(buffer);
-    
-    // Usa o número
-    Print("\nVoce digitou: ");
-    PrintNumber(numero);
+    return numero;
+}
+
+void main(void)
+{
+    int numero; 
+    // Limpa a tela
+    Screen(0);
+   
+    // para loops determinados
+    // for(int contador=0;contador<5;contador++)
+    // {
+    //     numero = lenumero();
+    //      // Usa o número
+    //     Print("\nVoce digitou: ");
+    //     PrintNumber(numero);
+    // }
+
+    // para loops indeterminados
+    while(1)
+    {
+        numero = lenumero();
+        // Verifica condição de saída
+        if(numero==0)
+        {
+            break; // Sai do loop
+        }
+        // Usa o número
+        Print("\nVoce digitou: ");
+        PrintNumber(numero);
+    }
 }
